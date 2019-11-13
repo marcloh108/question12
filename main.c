4ac59028e66ba88ea87dab72bf04a064a8dd0db6
marcloh108 <marc.loh@molisys.com>
2019-11-13 10:19:19 +0700

    Remove duplicates from a given array of integers
    Create functions for condition and printing
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+static int remove_duplicates(int* nums, int arr_size)
+{
+    if (arr_size <=1)
+    {
+        return arr_size;
+    }
+    int i=0, j, count = 1;
+    while (i<arr_size)
+    {
+        for(j=i+1;j<arr_size && nums[i]==nums[j];j++){}
+
+            if(j<arr_size)
+            {
+                nums[count++] = nums[j];
+            }
+            i = j;
+    }
+
+        return count;
+
+
+}
+
+int main()
+{
+    int nums[] = {1, 1, 2, 3, 4, 4, 5, 6, 6, 6};
+    int size = sizeof(nums)/sizeof(nums[0]);
+    printf("Original array:\n");
+    int i;
+    for(i=0; i<size; i++)
+    {
+        printf("%d ", nums[i]);
+    }
+    int count = remove_duplicates(nums, size);
+    printf("\nAfter removing duplicates from the above sorted array: \n");
+    for(i=0;i<count;i++)
+    {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    return 0;
+}
